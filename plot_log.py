17bbf07db4adde9198fa2668b035d50e041d67c6
fracho123 <55332372+fracho123@users.noreply.github.com>
2020-08-03 19:45:55 -0400

    Some housekeeping and implemented different models. Have yet to run code for different models.

--- plot_log.py
+++ plot_log.py
@@ -1,68 +1,72 @@
+#Used to plot entropy and generator/discriminator loss from the output.csv's
+
 import os
 import numpy as np
 import csv
 import matplotlib.pyplot as plt
 
 # Fill in these five parameters to plot the desired log file
-depth = [2]
+depth = [2,3,4]
 num_qubits = 3
-epochs = [100]
-batches = [100]
+epochs = [100,200,500,1000,2000,5000]
+batches = [100,200,300]
 bound = 7
+#model = [,]
 
 
 for i in range(len(depth)):
     for j in range(len(epochs)):
         for k in range(len(batches)):
-            name = str(depth[i]) + '_' + str(num_qubits) + '_' + str(epochs[j]) + '_' + str(batches[k]) + '_' + str(bound)
-            desired_folder = 'logs/' + name
+            #for mo in range(len(model)):
+                name = str(depth[i]) + '_' + str(num_qubits) + '_' + str(epochs[j]) + '_' + str(batches[k]) + '_' + str(bound)# + '_' + str(model[mo])
+                desired_folder = 'logs/' + name
 
-            # check to see if the file exists
-            if os.path.exists(desired_folder):
-                file = desired_folder + '/output.csv'
-                f = open(file)
-                csv_f = csv.reader(f)
-                next(csv_f)
-                next(csv_f)
-                epoch = np.zeros(shape=epochs[j])
-                loss_d = np.zeros(shape=epochs[j])
-                loss_g = np.zeros(shape=epochs[j])
-                entropy = np.zeros(shape=epochs[j])
-
-
-                # extract the data from the file
-                m = 0
-                for row in csv_f:
-                    if row:
-                        epoch[m] = row[0]
-                        loss_d[m] = row[1]
-                        loss_g[m] = row[2]
-                        entropy[m] = row[4]
-                        m += 1
+                # check to see if the file exists
+                if os.path.exists(desired_folder):
+                    file = desired_folder + '/output.csv'
+                    f = open(file)
+                    csv_f = csv.reader(f)
+                    next(csv_f)
+                    next(csv_f)
+                    epoch = np.zeros(shape=epochs[j])
+                    loss_d = np.zeros(shape=epochs[j])
+                    loss_g = np.zeros(shape=epochs[j])
+                    entropy = np.zeros(shape=epochs[j])
 
 
-                # Plot progress w.r.t the generator's and the discriminator's loss function
-                t_steps = np.arange(epochs[j])
-                plt.figure(figsize=(6, 5))
-                plt.title("Progress in the loss function")
-                plt.plot(t_steps, loss_g, label="Generator loss function", color='mediumvioletred', linewidth=2)
-                plt.plot(t_steps, loss_d, label="Discriminator loss function", color='rebeccapurple', linewidth=2)
-                plt.grid()
-                plt.legend(loc='best')
-                plt.xlabel('time steps')
-                plt.ylabel('loss')
-                plt.show()
-                #plt.savefig('logs/Generator_And_Discriminator_Loss_Plots/'+name+'.png')
+                    # extract the data from the file
+                    m = 0
+                    for row in csv_f:
+                        if row:
+                            epoch[m] = row[0]
+                            loss_d[m] = row[1]
+                            loss_g[m] = row[2]
+                            entropy[m] = row[4]
+                            m += 1
 
-                # Plot progress w.r.t relative entropy
-                plt.figure(figsize=(6, 5))
-                plt.title("Relative Entropy ")
-                plt.plot(epoch, entropy, color='mediumblue', lw=4, ls=':')
-                plt.grid()
-                plt.xlabel('time steps')
-                plt.ylabel('relative entropy')
-                plt.show()
-                #plt.savefig('logs/Entropy_Plots/'+name+'.png')
 
-            else:
-                print('Logs with desired parameters do not exist.')
+                    # Plot progress w.r.t the generator's and the discriminator's loss function
+                    t_steps = np.arange(epochs[j])
+                    plt.figure(figsize=(6, 5))
+                    plt.title("Progress in the loss function - " + name)
+                    plt.plot(t_steps, loss_g, label="Generator loss function", color='mediumvioletred', linewidth=2)
+                    plt.plot(t_steps, loss_d, label="Discriminator loss function", color='rebeccapurple', linewidth=2)
+                    plt.grid()
+                    plt.legend(loc='best')
+                    plt.xlabel('time steps')
+                    plt.ylabel('loss')
+                    plt.show()
+                    #plt.savefig('logs/Generator_And_Discriminator_Loss_Plots/'+name+'_loss.png')
+
+                    # Plot progress w.r.t relative entropy
+                    plt.figure(figsize=(6, 5))
+                    plt.title("Relative Entropy - " + name)
+                    plt.plot(epoch, entropy, color='mediumblue', lw=4, ls=':')
+                    plt.grid()
+                    plt.xlabel('time steps')
+                    plt.ylabel('relative entropy')
+                    plt.show()
+                    #plt.savefig('logs/Entropy_Plots/'+name+'_entr.png')
+
+                else:
+                    print('Logs with desired parameters do not exist.')
--- w_state.py
+++ w_state.py
@@ -5,74 +5,89 @@ import matplotlib.pyplot as plt
 
 snapbase = 'logs/'
 
-kk = [1]
-epoch = [100]
+
+kk = [2,3,4,5,6,7]
+epoch = [200]
 batch = [100]
 bounds = 7
 num_qubit = 3
+models = ['normal','log_normal']
 
 data = w_state_3q()
+
+# last parameter for Q-GAN method is a string to indicate which uncertainty model you want to test
+# 'normal' = Normal Distribution
+# 'log_normal' = Log-Normal Distribution
+# 'uniform' = Uniform Distribution
 
 for i in range(len(kk)):
     for j in range(len(epoch)):
         for k in range(len(batch)):
-            snap = str(kk[i])+'_3_'+str(epoch[j])+'_'+str(batch[k])+'_7'
-            address = snapbase+snap
-            os.mkdir(address)
-            qgan = QGAN_method(kk[i], num_qubit, epoch[j], batch[k], bounds, address, data)
+            for mo in range(len(models)):
+                snap = str(kk[i]) + '_' + str(num_qubit) + '_' + str(epoch[j]) + '_' + str(batch[k]) + '_' + str(bounds)\
+                       + '_' + str(models[mo])
+                address = snapbase+snap
+                if os.path.exists(address):
+                    continue
+                else:
+                    os.mkdir(address)
+                qgan = QGAN_method(kk[i], num_qubit, epoch[j], batch[k], bounds, address, data, models[mo])
 
-            # Plot progress w.r.t the generator's and the discriminator's loss function
-            t_steps = np.arange(epoch[j])
-            plt.figure(figsize=(6, 5))
-            plt.title("Progress in the loss function")
-            plt.plot(t_steps, qgan.g_loss, label="Generator loss function", color='mediumvioletred', linewidth=2)
-            plt.plot(t_steps, qgan.d_loss, label="Discriminator loss function", color='rebeccapurple', linewidth=2)
-            plt.grid()
-            plt.legend(loc='best')
-            plt.xlabel('time steps')
-            plt.ylabel('loss')
+                # Plot progress w.r.t the generator's and the discriminator's loss function
+                t_steps = np.arange(epoch[j])
+                plt.figure(figsize=(6, 5))
+                plt.title("Progress in the loss function - "+ snap)
+                plt.plot(t_steps, qgan.g_loss, label="Generator loss function", color='mediumvioletred', linewidth=2)
+                plt.plot(t_steps, qgan.d_loss, label="Discriminator loss function", color='rebeccapurple', linewidth=2)
+                plt.grid()
+                plt.legend(loc='best')
+                plt.xlabel('time steps')
+                plt.ylabel('loss')
 
-            name = str(kk[i]) + '_' + str(num_qubit) + '_' + str(epoch[j]) + '_' + str(batch[k]) + '_' + str(bounds) + '_GeneratorAndDiscriminator.png'
-            address = 'logs/Generator_And_Discriminator_Loss_Plots/' + name
-            plt.savefig(address)
-
-
-            # Plot progress w.r.t relative entropy
-            plt.figure(figsize=(6, 5))
-            plt.title("Relative Entropy ")
-            plt.plot(np.linspace(0, epoch[j], len(qgan.rel_entr)), qgan.rel_entr, color='mediumblue', lw=4, ls=':')
-            plt.grid()
-            plt.xlabel('time steps')
-            plt.ylabel('relative entropy')
-
-            name = str(kk[i]) + '_' + str(num_qubit) + '_' + str(epoch[j]) + '_' + str(batch[k]) + '_' + str(bounds) + '_Entropy.png'
-            address = 'logs/Entropy_Plots/' + name
-            plt.savefig(address)
+                name = str(kk[i]) + '_' + str(num_qubit) + '_' + str(epoch[j]) + '_' + str(batch[k]) + '_' + str(bounds)\
+                       + '_' + str(models[mo]) + '_loss.png'
+                address = 'logs/Generator_And_Discriminator_Loss_Plots/' + name
+                plt.savefig(address)
 
 
-            # Plot the PDF of the resulting distribution against the target distribution, i.e. log-normal
-            log_normal = data
-            # log_normal = np.round(log_normal)
-            # log_normal = log_normal[log_normal <= bounds[1]]
-            temp = []
-            for l in range(int(bounds + 1)):
-                temp += [np.sum(log_normal == l)]
-            log_normal = np.array(temp / sum(temp))
+                # Plot progress w.r.t relative entropy
+                plt.figure(figsize=(6, 5))
+                plt.title("Relative Entropy - " + snap)
+                plt.plot(np.linspace(0, epoch[j], len(qgan.rel_entr)), qgan.rel_entr, color='mediumblue', lw=4, ls=':')
+                plt.grid()
+                plt.xlabel('time steps')
+                plt.ylabel('relative entropy')
 
-            plt.figure(figsize=(6, 5))
-            plt.title("W-State QGAN")
-            samples_g, prob_g = qgan.generator.get_output(qgan.quantum_instance, shots=10000)
-            samples_g = np.array(samples_g)
-            samples_g = samples_g.flatten()
-            num_bins = len(prob_g)
-            plt.bar(samples_g, prob_g, color='royalblue', width=0.8, label='Simulation')
-            plt.plot(log_normal, '-o', label='W-State Measurements', color='deepskyblue', linewidth=4, markersize=12)
-            plt.xticks(np.arange(min(samples_g), max(samples_g) + 1, 1.0))
-            plt.grid()
-            plt.xlabel('states')
-            plt.ylabel('p(x)')
-            plt.legend(loc='best')
+                name = str(kk[i]) + '_' + str(num_qubit) + '_' + str(epoch[j]) + '_' + str(batch[k]) + '_' + str(bounds)\
+                       + '_' + str(models[mo]) + '_entr.png'
+                address = 'logs/Entropy_Plots/' + name
+                plt.savefig(address)
 
-            name = str(kk[i]) + '_' + str(num_qubit) + '_' + str(epoch[j]) + '_' + str(batch[k]) + '_' + str(bounds) + '_ProbPlot.png'
-            address = 'logs/Probability_Plots/' + name
-            plt.savefig(address)
+
+                # Plot the PDF of the resulting distribution against the target distribution, i.e. log-normal
+                log_normal = data
+                # log_normal = np.round(log_normal)
+                # log_normal = log_normal[log_normal <= bounds[1]]
+                temp = []
+                for l in range(int(bounds + 1)):
+                    temp += [np.sum(log_normal == l)]
+                log_normal = np.array(temp / sum(temp))
+
+                plt.figure(figsize=(6, 5))
+                plt.title("W-State QGAN - "+ snap)
+                samples_g, prob_g = qgan.generator.get_output(qgan.quantum_instance, shots=10000)
+                samples_g = np.array(samples_g)
+                samples_g = samples_g.flatten()
+                num_bins = len(prob_g)
+                plt.bar(samples_g, prob_g, color='royalblue', width=0.8, label='Simulation')
+                plt.plot(log_normal, '-o', label='W-State Measurements', color='deepskyblue', linewidth=4, markersize=12)
+                plt.xticks(np.arange(min(samples_g), max(samples_g) + 1, 1.0))
+                plt.grid()
+                plt.xlabel('states')
+                plt.ylabel('p(x)')
+                plt.legend(loc='best')
+
+                name = str(kk[i]) + '_' + str(num_qubit) + '_' + str(epoch[j]) + '_' + str(batch[k]) + '_' + str(bounds)\
+                       + '_' + str(models[mo]) + '_plot.png'
+                address = 'logs/Probability_Plots/' + name
+                plt.savefig(address)
